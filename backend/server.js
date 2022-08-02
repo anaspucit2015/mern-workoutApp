08401e5c375c6e9d481c45907922c2cb9eff5d30
zubair-haider <anas.saleem@gigalabs.co>
2022-08-02 17:27:04 +0500

    New data added

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const workoutRoutes = require('./routes/workout')
 app.use(express.json())
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -20,7 +20,6 @@ app.use((req, res, next) => {
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-  console.log(req.path, req.method)
   next()
 })
 
