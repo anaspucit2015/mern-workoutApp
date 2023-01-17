b5fbcaad5ef1f758c3d804567a19cad50eafd2bc
zubair-haider <anas.saleem@gigalabs.co>
2023-01-18 02:49:04 +0500

    Form added and context api added

--- frontend/src/components/WorkoutForm/index.js
+++ frontend/src/components/WorkoutForm/index.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import { Container, Title, Form, Label, Input } from './styles'
-const WorkoutForm = ({ formData, handleValueChange }) => {
+import { Container, Title, Form, Label, Input, AddButton } from './styles'
+const WorkoutForm = ({ formData, handleValueChange, handleSubmit }) => {
   const { title, reps, load } = formData
   return (
     <Container>
       <Title>Creat Workout</Title>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Label>Exercise Title</Label>
         <Input
           type='text'
           name='title'
           value={title}
           placeholder='Title'
+          onChange={({ target: { name, value } }) =>
+            handleValueChange(name, value)
+          }
+        />
+        <Label>Exercise Load (Kgs)</Label>
+        <Input
+          type='number'
+          name='load'
+          value={load}
+          placeholder='Load'
           onChange={({ target: { name, value } }) =>
             handleValueChange(name, value)
           }
@@ -26,16 +36,7 @@ const WorkoutForm = ({ formData, handleValueChange }) => {
             handleValueChange(name, value)
           }
         />
-        <Label>Exercise Title</Label>
-        <Input
-          type='number'
-          name='load'
-          value={load}
-          placeholder='Load'
-          onChange={({ target: { name, value } }) =>
-            handleValueChange(name, value)
-          }
-        />
+        <AddButton type='submit'>Add Exercise</AddButton>
       </Form>
     </Container>
   )
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -2,24 +2,50 @@ import React, { useEffect, useState } from 'react'
 import { Container, CardContainer } from './styles'
 import WorkoutDetails from '../../components/WorkoutDetails'
 import WorkoutForm from '../../components/WorkoutForm'
+import { ToastContainer, toast } from 'react-toastify'
+import { useWorkoutsContext } from '../../hooks/useWorkoutsContext'
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null)
   const [formData, setFormData] = useState({ title: '', reps: 0, load: 0 })
+  const { workouts, dispatch } = useWorkoutsContext()
 
   useEffect(() => {
-    const fetchWorkouts = async () => {
-      const data = await fetch('http://localhost:4000/api/workouts')
-        .then(res => res.json())
-        .then(data => data)
-
-      setWorkouts(data)
-    }
     fetchWorkouts()
   }, [])
 
+  const fetchWorkouts = async () => {
+    await fetch('http://localhost:4000/api/workouts')
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'SET_WORKOUTS', payload: data }))
+      .catch(err => {
+        toast.error('Something went wrong')
+        console.log('err: ', err)
+      })
+  }
+
   const handleValueChange = (key, value) => {
     setFormData({ ...formData, [key]: value })
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      const res = await fetch('http://localhost:4000/api/workouts', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json())
+      if (res.error) {
+        throw res.error
+      }
+      toast.success('Workout added')
+      setFormData({ title: '', reps: 0, load: 0 })
+      dispatch({ type: 'CREATE_WORKOUT', payload: res })
+    } catch (err) {
+      toast.error(err)
+    }
   }
   return (
     <Container>
@@ -29,7 +55,12 @@ const Home = () => {
             <WorkoutDetails key={workout._id} workout={workout} />
           ))}
       </CardContainer>
-      <WorkoutForm formData={formData} handleValueChange={handleValueChange}/>
+      <WorkoutForm
+        formData={formData}
+        handleValueChange={handleValueChange}
+        handleSubmit={handleSubmit}
+      />
+      <ToastContainer />
     </Container>
   )
 }
